437dff7be9b7744a9a97ea1914077fc020113ca0
RabbagastRobert <robertandrelarsen@gmail.com>
2025-09-25 11:32:49 +0200

    Added comments to script functions

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-// Elements
+////////// ELEMENTS
 
+// Hamburger menu
 const hamburger = document.getElementById('hamburger');
 const hamburgerButton = document.getElementById('hamburgerButton');
 const hamburgerMenu = document.getElementById('hamburgerMenu');
 
+// Fake social media contacts
 const fakeLinks = document.querySelectorAll('.fakeLinks');
 const fakeContacts = document.getElementById('fakeContacts');
 const closeFakeContacts = document.getElementById('closeFakeContacts');
 
-// Event listeners
+////////// EVENT LISTENERS
 
+// Records the time an element is set as visible so we can delay the removal of the element
 let removeDelay = -1;
 
+// Toggles the hamburgermenu
 hamburgerButton.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('active');
     removeDelay = Date.now();
 });
 
+// Toggles the message box for the fake socials
 fakeLinks.forEach((element) => element.addEventListener('click', () => {
     fakeContacts.classList.toggle('active');
     removeDelay = Date.now();
 }));
 
+// Hides hamburger menu and socials message box when clicking outside of the element
 window.addEventListener('click', (ev) => {
     if (Date.now() - removeDelay < 200) return;
     if (ev.target === hamburger || ev.target === fakeLinks || ev.target === fakeContacts) return;
@@ -35,6 +41,7 @@ window.addEventListener('click', (ev) => {
     fakeContacts.classList.remove('active');
 });
 
+// Close button for socials message box
 closeFakeContacts.addEventListener('click', () => fakeContacts.classList.remove('active'));
 
 
